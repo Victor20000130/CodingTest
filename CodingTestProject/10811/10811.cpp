20ad26e5ffc52fdf017e744226699c3551dbdea7
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-01-06 01:55:31 +0900

    update: 코드 스타일 개선 및 불필요한 파일 삭제

--- CodingTestProject/10811/10811.cpp
+++ CodingTestProject/10811/10811.cpp
@@ -5,20 +5,24 @@
 using namespace std;
 const int MAXCOUNT = 100;
 //내림차순 정렬이 아니라서...다시 짜야해...
-int main() {
+int main()
+{
 	int n, m, stidx, edidx;
 	vector<int> v;
 
 	int arr[MAXCOUNT];
-	for (int a = 0; a < MAXCOUNT; a++) {
+	for (int a = 0; a < MAXCOUNT; a++)
+	{
 		arr[a] = 0;
 	}
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		arr[i] = i + 1;
 	}
 
-	for (int j = 0; j < m; j++) {
+	for (int j = 0; j < m; j++)
+	{
 		cin >> stidx >> edidx;
 		stidx -= 1;
 		edidx -= 1;
@@ -27,7 +31,8 @@ int main() {
 		// sort(v.begin() + stidx, v.begin() + edidx, greater<int>());
 	}
 
-	for (int k = 0; k < v.capacity(); k++) {
+	for (int k = 0; k < v.capacity(); k++)
+	{
 		// cout << v[k] << ' ';
 	}
 
