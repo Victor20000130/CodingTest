f7cfb3412ebdac384fbc1ac9123cd997d411fdbe
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-01-06 01:47:45 +0900

    update: 테스트 케이스 추가 및 불필요한 파일 삭제

--- CodingTestProject/10811/10811.cpp
+++ CodingTestProject/10811/10811.cpp
@@ -3,20 +3,32 @@
 #include <vector>
 
 using namespace std;
-
+const int MAXCOUNT = 100;
+//내림차순 정렬이 아니라서...다시 짜야해...
 int main() {
 	int n, m, stidx, edidx;
 	vector<int> v;
 
+	int arr[MAXCOUNT];
+	for (int a = 0; a < MAXCOUNT; a++) {
+		arr[a] = 0;
+	}
 	cin >> n >> m;
-
 	for (int i = 0; i < n; i++) {
-		v.push_back(i + 1);
+		arr[i] = i + 1;
 	}
 
 	for (int j = 0; j < m; j++) {
 		cin >> stidx >> edidx;
-		sort(v.begin() + stidx - 1, v.begin() + edidx, greater<int>());
+		stidx -= 1;
+		edidx -= 1;
+
+		sort(arr + stidx, arr + edidx, greater<int>());
+		// sort(v.begin() + stidx, v.begin() + edidx, greater<int>());
+	}
+
+	for (int k = 0; k < v.capacity(); k++) {
+		// cout << v[k] << ' ';
 	}
 
 }
