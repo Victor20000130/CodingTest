fdabd9eaaccb8688a6fa9805fe478fa0151116d0
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-08-05 21:54:02 +0900

    Add build response and progress files for project 10808
    - Created objects1.rsp to track object files for 10808.cpp
    - Added progress.make to manage build progress for the project

---
+++ build/CMakeFiles/10808.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Users/qpalz/Desktop/AllStudy/CodingTest/CodingTestProject/10808/test_cases/10808/10808.exe"
+  "C:/Users/qpalz/Desktop/AllStudy/CodingTest/CodingTestProject/10808/test_cases/10808/10808.exe.manifest"
+  "C:/Users/qpalz/Desktop/AllStudy/CodingTest/CodingTestProject/10808/test_cases/10808/10808.pdb"
+  "CMakeFiles/10808.dir/CodingTestProject/10808/10808.cpp.obj"
+  "CMakeFiles/10808.dir/CodingTestProject/10808/10808.cpp.obj.d"
+  "lib10808.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10808.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
