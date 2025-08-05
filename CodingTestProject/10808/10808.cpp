c42c917bb8a8d64ae6234afae2a91c1967bc34ef
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-08-05 22:02:20 +0900

    코드 정리: 불필요한 주석 제거 및 문자 빈도 계산 로직 개선

--- CodingTestProject/10808/10808.cpp
+++ CodingTestProject/10808/10808.cpp
@@ -6,16 +6,30 @@ int main()
 {
     ios::sync_with_stdio();
     cin.tie(0);
-    string str;
-    cin >> str;
-    vector<char> v1;
-    vector<int> v2(26);
-    for (int i = 97; i < 123; i++)
-        v1.push_back(i);
+    // string str;
+    // cin >> str;
+    // vector<char> v1;
+    // vector<int> v2(26);
+    // for (int i = 97; i < 123; i++)
+    //     v1.push_back(i);
+    // for (int i = 0; i < 26; i++)
+    //     for (int j = 0; j < str.length(); j++)
+    //         if (str[j] == v1[i])
+    //             v2[i]++;
+    // for (int e : v2)
+    //     cout << e << ' ';
+    int freq[26] = {};
+    string s;
+    cin >> s;
+
+    for (auto c : s)
+    {
+        freq[c - 'a']++;
+    }
     for (int i = 0; i < 26; i++)
-        for (int j = 0; j < str.length(); j++)
-            if (str[j] == v1[i])
-                v2[i]++;
-    for (int e : v2)
-        cout << e << ' ';
+    {
+        cout << freq[i] << ' ';
+    }
+
 }
+
